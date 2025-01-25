ba9376d1bb166dbb16fff374091070d4c01c98d9
simoZak <zakhnini92@gmail.com>
2025-01-26 00:14:30 +0100

    checking value if integer or float

---
+++ functions.py
@@ -0,0 +1,13 @@
+def isInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+def isFloat(s):
+    try: 
+        float(s)
+        return True
+    except ValueError:
+        return False
